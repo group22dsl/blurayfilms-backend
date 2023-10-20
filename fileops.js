e7a599ebd3a5bf77da83e02646efc0b870ae0f6e
bimallakshitha <bimallakshitha@gmail.com>
2023-10-20 23:33:09 +1100

    google cloud storage integration.
    - downloads the file from subtitle website
    - uploads to google cloud storage bucket

---
+++ fileops.js
@@ -0,0 +1,120 @@
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const { Storage } = require('@google-cloud/storage');
+const axios = require('axios');
+const path = require('path')
+
+
+const bucketName = 'bluraymoviesubtitles';
+
+async function uploadFile(
+    fileId,
+    filePath,
+    destFileName,
+    generationMatchPrecondition = 0
+) {
+
+    // Creates a client
+    const storage = new Storage({ keyFilename: './application_default_credentials.json' });
+
+    async function uploadFile() {
+
+        const returnFilePath = filePath;
+
+        const bucket = storage.bucket(bucketName);
+        const standardFileName = getStandardCloudFileName(fileId,destFileName);
+        const tempFile = `./tmp/${standardFileName}`;
+        await download(filePath, tempFile);
+        let file = bucket.file(standardFileName);
+        const fileExists = await file.exists();
+        if (!fileExists[0]) {
+            const options = {
+                destination: standardFileName,
+                preconditionOpts: { ifGenerationMatch: generationMatchPrecondition },
+            };
+            //TODO: set file extension as metadata
+            file = (await bucket.upload(tempFile, options)).file;
+        }
+        fs.unlinkSync(tempFile);
+        return getCloudFileUrl(fileId);
+
+    }
+
+    uploadFile().catch(console.error);
+}
+
+function getStandardCloudFileName(fileID, fileName=null){
+    return `${fileID}`;
+}
+
+async function getCloudFileUrlIfExists(fileID, fileName){
+    const storage = new Storage({ keyFilename: './application_default_credentials.json' });
+    const bucket = storage.bucket(bucketName);
+    const file = bucket.file(getStandardCloudFileName(fileID, fileName));
+    const fileExists = await file.exists();
+    if (fileExists[0]) {
+        return getCloudFileUrl(fileID);
+    }
+    return null;
+}
+
+async function getCloudFileUrl(fileID) {
+    const config = {
+        version: 'v4',
+        action: 'read',
+        expires: Date.now() + 1000 * 60 * 60, // valid for one hour
+    }
+
+    const storage = new Storage({ keyFilename: './application_default_credentials.json' });
+    const bucket = storage.bucket(bucketName);
+    const file = bucket.file(getStandardCloudFileName(fileID));
+    const fileExists = await file.exists();
+    if (fileExists[0]) {
+        console.log("file exists");
+    }
+    file.getSignedUrl(config, function (err, url) {
+        if (err) {
+            console.error(err);
+            return null;
+        }
+        return url;
+        console.log("Url is : " + url);
+    });
+}
+
+async function download(url, filePath) {
+    const proto = !url.charAt(4).localeCompare('s') ? https : http;
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(filePath);
+        let fileInfo = null;
+
+        const request = proto.get(url, response => {
+            if (response.statusCode !== 200) {
+                fs.unlink(filePath, () => {
+                    reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+                });
+                return;
+            }
+
+            fileInfo = {
+                mime: response.headers['content-type'],
+                size: parseInt(response.headers['content-length'], 10),
+            };
+
+            response.pipe(file);
+        });
+
+        file.on('finish', () => resolve(fileInfo));
+        request.on('error', err => {
+            fs.unlink(filePath, () => reject(err));
+        });
+        file.on('error', err => {
+            fs.unlink(filePath, () => reject(err));
+        });
+
+        request.end();
+    });
+}
+
+module.exports = { uploadFile, getCloudFileUrlIfExists };
--- server.js
+++ server.js
@@ -68,6 +68,62 @@ app.get('/api/getSubtitles', async (req, res) => {
     }
 });
 
+app.get('/api/getSubtitleDownloadLinkForFile', async (req, res) => {
+
+    const baseUrl = process.env.OPEN_SUBTITLE_DOWNLOAD_BASE_URL;
+    const fileOperations = require('./fileops');
+
+    const fileID = req.query.fileID;
+    const cloudFileUrl = await fileOperations.getCloudFileUrlIfExists(fileID);
+    if(cloudFileUrl != null){
+        console.log("hitting if")
+        return cloudFileUrl;
+    }
+    else {
+        console.log("hitting else")
+        console.log(fileID)
+        //file not found in google cloud storage
+        const params = {
+            file_id: fileID
+        };
+    
+        try {
+            // const response = await axios.post('https://api.opensubtitles.com/api/v1/download',params,{
+            //     headers: {
+            //         'Api-Key': process.env.OPEN_SUBTITLE_API_KEY,
+            //         'User-Agent': 'blurayfilms v1.0'
+            //     }
+            // });
+            
+            //Comment out the below constant and rename all the response_data_data to response.data.data. Also uncomment the above axios post call
+            const response_data_data = {
+                "link": "https://www.opensubtitles.com/download/D3FB6A57F3C7BF5A427C1B4C9615732FE1A407D87643BD4FAD4B66E962727AD5FC1154C0819A220180FFD3574D6C80F7E0736499E33F056B2B2B4E5834C02A33DA7C3C169A772594C4879A4ADC5CEBF9A75D3A1A8AFEAA8579CABAE0C5969A8008016A3C6C6E043F7BF4EB461F14AFB94BAB41DA60E8EC9E585A2A60ADD89E76AC98F9002076C9C7C46B34AF68A5B3315DB9AF33E91542559BBADD04584C4CF251670139CA6C55379FF4C79FC789C0CEFA42612F40C93929C67A1BF012952E3882AD5B9467CD7F2D304060BA2055480374B25D444A643FC1F96B55A63FEF18AFF1C9CF7DDBBC9F9E93EA007BD93053C2519B6D1C8E1B5B4DF2B7A5F01CEA561477A8C7A9AC5327D7FC2DB541F0A15F2F4A225FAAE4256BF289AC9B9095AFA3E4B3B7B48CA007AA20/subfile/Iron.Man.3.2013.720p.BluRay.x264-SPARKS.srt",
+                "file_name": "Iron.Man.3.2013.720p.BluRay.x264-SPARKS.srt",
+                "requests": 2,
+                "remaining": 98,
+                "message": "Your quota will be renewed in 13 hours and 17 minutes (2023-10-20 23:59:59 UTC) ts=1697798539 ",
+                "reset_time": "13 hours and 17 minutes",
+                "reset_time_utc": "2023-10-20T23:59:59.000Z",
+                "uk": "app_64110",
+                "uid": 2,
+                "ts": 1697798539
+            };
+            if (response_data_data) {
+                const fileDownloadUrl = fileOperations.uploadFile(fileID, response_data_data.link,response_data_data.file_name) ;
+                res.json({ message: fileDownloadUrl });
+                return;
+            } else {
+                res.json({ message: null });
+                return;
+            }
+        } catch (error) {
+            console.log(error);
+            res.json({ message: 'bimal' });
+            return;
+        }
+    }
+});
+
 app.get('/api/getMoviesListByType', async (req, res) => {
     try {
         if(req.query.searchType == '') {
